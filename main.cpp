0aacb777665e4dfcc774097ee5680964794869b3
aaravjain22 <ajvid220512@gmail.com>
2026-07-17 21:27:56 +0530

    Variable access time from heap,Added Stats

--- main.cpp
+++ main.cpp
@@ -3,24 +3,29 @@
 
 int main()
 {
-     int* my_var = new int(32);
-     int* copy_myvar;
+     int* my_var = new int(32); // allocated my_var on heap
+     int copy_myvar = 0;
 
     const int iters = 100000000;
 
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now(); //started count
+
     for (int i = 0 ; i < iters ; ++i){
-        copy_myvar = my_var;
+        copy_myvar = *my_var;
     }
 
-    auto stop = std::chrono::high_resolution_clock::now();
+    auto stop = std::chrono::high_resolution_clock::now(); //stopped count
 
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start);
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start); //deducing duration from stop - start
 
-    double av_dura = static_cast<double>(duration.count())/iters;
+    double av_dura = static_cast<double>(duration.count())/iters; //got averge duration per iteration
     std::cout << av_dura << std::endl;
-    /*  output = 0.3154ns on compilation with "-01"
-        2.5152ns withought optimization*/
+   
+    delete my_var; //deleted my_var 
+
+     /* stats 
+    output = 0.28024ns on compilation with "-01"
+    1.93818ns withought optimization*/
 
     return 0;
 }
