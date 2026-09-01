e59aeb1ef37875786e1069c773474d81321c21e1
Aarav Jain <ajvid220512@gmail.com>
2026-09-01 12:50:18 +0530

    Add chrono.cpp to display current time

---
+++ chrono.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+int main(){
+	auto now =std::chrono::system_clock::now();
+	std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+	std::cout << std::ctime(&currentTime);
+	return 0;
+}
